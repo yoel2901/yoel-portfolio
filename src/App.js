a4d54e1bd936fce32d27baf1dd86beb5d5f7b385
yoel2901 <yoel.telouk@gmail.com>
2026-04-20 16:01:03 +0200

    fix unused Contact import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
-import Contact from "./components/Contact";
+
 
 function App() {
   return (
